86edd912b87ede91e4984665eb40a0e62c665b18
Ranadheer <guntiranadheer11@gmail.com>
2025-03-15 23:03:20 +0530

    file

---
+++ src/Components/Data.jsx
@@ -0,0 +1,65 @@
+
+  export  const data=[
+        {
+            id: 1,
+            name: 'Shiva kumar',
+            you: "you",
+            fllowers: "followers",
+            manage:"manage followers",
+            img:"image/tag",
+            title: "frontend engineer specilizling MERN,MEAN stack...",
+            Description: " LISTENUP YOUNG DEVS:A REAL TALK ABOUT AI & CAREERS",
+            reactions: {
+                likeButton: "like",
+                comment: "comment",
+                report: "report",
+                send: "send",
+                impression: "impressions",
+                views: "view"
+            }
+        },
+    
+        {
+            id: 2,
+            name: 'Shiva kumar',
+            you: "you",
+            img: "image/tag",
+            title: "frontend engineer specilizling MERN,MEAN stack...",
+            Description: "WHY SOUTH INDIAN TECH EDUCATION FAILING IN 2025",
+            reactions: {
+                likeButton: "like",
+                comment: "comment",
+                report: "report",
+                send: "send",
+                impression: "impressions",
+                views: "view"
+            }
+    
+    
+        },
+        {
+            id: 3,
+            name: 'Shiva kumar',
+            you: "you",
+            img: "image/tag",
+            title: "frontend engineer specilizling MERN,MEAN stack...",
+            Description: "FINALLY LAUNCHNED MY EDTECH Startup DevGan:Buliding Real Engineering",
+            reactions: {
+                likeButton: "like",
+                comment: "comment",
+                report: "report",
+                send: "send",
+                impression: "impressions",
+                views: "view"
+            }
+    
+        },
+    ]
+ 
+
+// const details = ["id:1", "name:Shiva kumar", "title:frontend engineer specilizling MERN,MEAN stack...",]
+// const description = ["Description: LISTENUP YOUNG DEVS:A REAL TALK ABOUT AI & CAREERS"]
+// const reactions = [['like', 'comments', 'report'], ['comment', 'repost']]
+// const impressions = ['impressions']
+// const views = ['views']
+// const other = ['more']
---
+++ src/Components/Page1.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Data from './Components/Data'
+ function Page1() {
+  return (
+    <div>
+        <pre>
+        <h1>All activity</h1>
+        
+        <hr />
+        </pre>
+        <h1> hii</h1>
+    </div>
+  )
+}
+export default Page1
